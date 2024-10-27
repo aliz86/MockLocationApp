12b3cf10aa85f1d976a47b4d43a6ead3ca4eb4fb
aliz86 <seaworld67@aut.ac.ir>
2024-10-27 13:54:07 +1100

    Update LocationMockHelper.kt : 1. fow apis lower than 31 (Android 11) 2. better code for setting location fom both GPS and NETWORK providers.

--- app/src/main/java/com/dataliz/gpsmock/domain/helpers/LocationMockHelper.kt
+++ app/src/main/java/com/dataliz/gpsmock/domain/helpers/LocationMockHelper.kt
@@ -1,67 +1,38 @@
 package com.dataliz.gpsmock.domain.helpers
 
+import android.location.Criteria
 import android.location.Location
 import android.location.LocationManager
 import android.location.provider.ProviderProperties
 import android.os.Build
+import android.os.PowerManager
 import android.os.SystemClock
 import android.util.Log
 import com.google.android.gms.maps.model.LatLng
 
 class LocationMockHelper {
     fun startLocationMocking(locationManager: LocationManager, targetLocation: LatLng) {
+        mockLocationForProvider(LocationManager.NETWORK_PROVIDER, locationManager, targetLocation)
+        mockLocationForProvider(LocationManager.GPS_PROVIDER, locationManager, targetLocation)
+    }
+    fun mockLocationForProvider(provider: String, locationManager: LocationManager, targetLocation: LatLng) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             try {
-                locationManager.addTestProvider(
-                    LocationManager.GPS_PROVIDER,
-                    false, // requiresNetwork
-                    false, // requiresSatellite
-                    false, // requiresCell
-                    false, // hasMonetaryCost
-                    false, // supportsAltitude
-                    true, // supportsSpeed
-                    false, // supportsBearing
-                    ProviderProperties.POWER_USAGE_LOW, // powerRequirement
-                    ProviderProperties.ACCURACY_FINE // accuracy
-                )
-
-                locationManager.addTestProvider(
-                    LocationManager.GPS_PROVIDER,
-                    false, // requiresNetwork
-                    false, // requiresSatellite
-                    false, // requiresCell
-                    false, // hasMonetaryCost
-                    false, // supportsAltitude
-                    true, // supportsSpeed
-                    false, // supportsBearing
-                    ProviderProperties.POWER_USAGE_MEDIUM, // powerRequirement
-                    ProviderProperties.ACCURACY_FINE // accuracy
-                )
-                locationManager.addTestProvider(
-                    LocationManager.GPS_PROVIDER,
-                    false, // requiresNetwork
-                    false, // requiresSatellite
-                    false, // requiresCell
-                    false, // hasMonetaryCost
-                    true, // supportsAltitude
-                    true, // supportsSpeed
-                    false, // supportsBearing
-                    ProviderProperties.POWER_USAGE_HIGH, // powerRequirement
-                    ProviderProperties.ACCURACY_FINE // accuracy
-                )
-
-                locationManager.addTestProvider(
-                    LocationManager.NETWORK_PROVIDER,
-                    false, // requiresNetwork
-                    false, // requiresSatellite
-                    false, // requiresCell
-                    false, // hasMonetaryCost
-                    true, // supportsAltitude
-                    true, // supportsSpeed
-                    false, // supportsBearing
-                    ProviderProperties.POWER_USAGE_MEDIUM, // powerRequirement
-                    ProviderProperties.ACCURACY_FINE // accuracy
-                )
+                locationManager.setTestProviderEnabled(provider, true)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    locationManager.addTestProvider(
+                        provider,
+                        false, // requiresNetwork
+                        false, // requiresSatellite
+                        false, // requiresCell
+                        false, // hasMonetaryCost
+                        true, // supportsAltitude
+                        true, // supportsSpeed
+                        false, // supportsBearing
+                        ProviderProperties.POWER_USAGE_HIGH, // powerRequirement
+                        ProviderProperties.ACCURACY_FINE // accuracy
+                    )
+                }
             } catch (e: SecurityException) {
                 // Handle the case where the app is not allowed to mock location
                 // This might happen if the user hasn't enabled mock locations
@@ -69,22 +40,23 @@ class LocationMockHelper {
                 Log.e("MapViewModel", "Error setting up mock location", e)
                 return
             }
-            locationManager.setTestProviderEnabled(LocationManager.GPS_PROVIDER, true)
-            locationManager.setTestProviderEnabled(LocationManager.NETWORK_PROVIDER, true)
 
-            val mockLocation = Location(LocationManager.GPS_PROVIDER)
-            mockLocation.latitude = targetLocation.latitude
-            mockLocation.longitude = targetLocation.longitude
-            mockLocation.accuracy = 10f // Set desired accuracy
-            mockLocation.time = System.currentTimeMillis()
-            mockLocation.elapsedRealtimeNanos = SystemClock.elapsedRealtimeNanos()
-
-            locationManager.setTestProviderLocation(LocationManager.NETWORK_PROVIDER, mockLocation)
-            locationManager.setTestProviderLocation(LocationManager.GPS_PROVIDER, mockLocation)
+            locationManager.setTestProviderLocation(provider, createMockLocation(provider, targetLocation))
         }
     }
 
     fun stopLocationMocking(locationManager: LocationManager) {
         locationManager.removeTestProvider(LocationManager.GPS_PROVIDER)
+    }
+
+    private fun createMockLocation(provider: String, targetLocation: LatLng): Location {
+        val mockLocation = Location(provider)
+        return mockLocation.apply {
+            latitude = targetLocation.latitude
+            longitude = targetLocation.longitude
+            accuracy = 10f // Set desired accuracy
+            time = System.currentTimeMillis()
+            elapsedRealtimeNanos = SystemClock.elapsedRealtimeNanos()
+        }
     }
 }
